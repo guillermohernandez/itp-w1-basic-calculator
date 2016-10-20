862127c72b915c9beb3408ed4752e2c6bca82068
Santiago Basulto <santiago.basulto@gmail.com>
2016-10-20 13:16:26 -0300

    Solution implemented

--- basic_calculator/main.py
+++ basic_calculator/main.py
@@ -2,4 +2,13 @@
 
 
 def basic_calculator(num1, num2, operation):
-    pass
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        return num1 / num2
+    else:
+        return 'Invalid operation'
